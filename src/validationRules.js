f0d279d32577114fd17387b0696c548e66629590
Dominik Sumer <domi.sumer@gmail.com>
2017-08-23 00:02:59 +0200

    initially added validationRules

---
+++ src/validationRules.js
@@ -0,0 +1,29 @@
+const rules = {
+    isRequired: (values, value) => {
+        return !!value;
+    },
+    maxLength: (values, value, maxLength) => {
+        return (!value || value.length <= maxLength);
+    },
+    equals: (values, value, condition) => {
+        return value === condition;
+    },
+    equalsField: (values, value, fieldName) => {
+        return values[fieldName] === value;
+    },
+    equalsFields: (values, value, fieldNames) => {
+        let allValid = true;
+        for (let name of fieldNames) {
+            if (values[name] !== value) {
+                allValid = false;
+            }
+        }
+        return allValid;
+    }
+};
+// TODO: add more basic rules
+
+rules.equalsField.createsDependencies = true;
+rules.equalsFields.createsDependencies = true;
+
+export default rules;
