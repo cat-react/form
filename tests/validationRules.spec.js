b1d5e0415beb3a5fb53326d78b40c95ce8483d9d
Dominik Sumer <domi.sumer@gmail.com>
2017-08-23 01:29:41 +0200

    added codecov

--- tests/validationRules.spec.js
+++ tests/validationRules.spec.js
@@ -1,16 +1,18 @@
 import rules from '../src/validationRules';
 
-describe('ValidationRule - isRequired', () => {
-    it('should successfully validate the value', () => {
-        const valid = rules.isRequired(null, 'abc');
-        expect(valid).toBe(true);
-    });
-    it('should invalidate the value', () => {
-        let valid = rules.isRequired(null, '');
-        expect(valid).toBe(false);
-        valid = rules.isRequired(null, null);
-        expect(valid).toBe(false);
-        valid = rules.isRequired(null, undefined);
-        expect(valid).toBe(false);
+describe('Validation Rules', () => {
+    describe('isRequired', () => {
+        it('should successfully validate the value', () => {
+            const valid = rules.isRequired(null, 'abc');
+            expect(valid).toBe(true);
+        });
+        it('should invalidate the value', () => {
+            let valid = rules.isRequired(null, '');
+            expect(valid).toBe(false);
+            valid = rules.isRequired(null, null);
+            expect(valid).toBe(false);
+            valid = rules.isRequired(null, undefined);
+            expect(valid).toBe(false);
+        });
     });
 });
