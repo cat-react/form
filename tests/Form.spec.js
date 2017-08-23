15c630207c33534e0fc4c33e27b53925c27c79ea
Dominik Sumer <domi.sumer@gmail.com>
2017-08-23 14:32:20 +0200

    added test for addvalidationrule

--- tests/Form.spec.js
+++ tests/Form.spec.js
@@ -8,4 +8,12 @@ describe('Form', () => {
         expect(wrapper.contains(<span className="abc">abc</span>)).toBe(true);
         expect(wrapper.is('.myForm')).toBe(true);
     });
+
+    it('should add the vaidationRule', () => {
+        Form.addValidationRule("testRule", (values, value) => {
+            return value === 'abc';
+        }, false);
+        expect(Form.validationRules.testRule(null, 'abc')).toBe(true);
+        expect(Form.validationRules.testRule(null, 'ac')).toBe(false);
+    });
 });
