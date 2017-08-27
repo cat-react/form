c9b4d739a438e8a71c1d77ddb8c459171d629d04
Dominik Sumer <domi.sumer@gmail.com>
2017-08-27 21:37:15 +0200

    added examples directory and run script with webpack dev server

---
+++ examples/webpack.config.js
@@ -0,0 +1,42 @@
+var fs = require('fs');
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'inline-source-map',
+    entry: fs.readdirSync(__dirname).reduce(function (entries, dir) {
+        const isDir = fs.lstatSync(path.join(__dirname, dir)).isDirectory();
+
+        if (isDir) {
+            entries[dir] = path.join(__dirname, dir, 'app.js');
+        }
+
+        return entries;
+    }, {}),
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: '[name].js',
+        chunkFilename: '[id].chunk.js',
+        publicPath: '/build/'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            '@cat-react/form': '../../src'
+        }
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin('vendor.bundle')
+    ]
+};
