b01257ccb660778754e7e7de0bb25e84802db77e
Dominik Sumer <domi.sumer@gmail.com>
2017-09-25 20:13:39 +0200

    updated favicon and added logo to examples

--- examples/Home/index.js
+++ examples/Home/index.js
@@ -40,16 +40,23 @@ export default class extends React.Component {
         return (
             <div>
                 <div className="jumbotron">
-                    <h1 className="display-3">Welcome!</h1>
-                    <p className="lead">
-                        These are the examples of <a href="https://github.com/cat-react/form">@cat-react/form</a>.
-                    </p>
-                    <hr />
-                    <p>
-                        Use the navigation on the left side to take a closer look at each example.
-                    </p>
+                    <div className="row">
+                        <div className="col-md-9">
+                            <h1 className="display-3">Welcome!</h1>
+                            <p className="lead">
+                                These are the examples of <a href="https://github.com/cat-react/form">@cat-react/form</a>.
+                            </p>
+                            <hr />
+                            <p>
+                                Use the navigation on the left side to take a closer look at each example.
+                            </p>
+                        </div>
+                        <div className="logo col-md-3">
+                            <a href="https://github.com/cat-react"><img src="/logo.png" /></a>
+                        </div>
+                    </div>
                 </div>
-                <div>                    
+                <div>
                     <h3>Try the library live!</h3>
                     <LiveProvider code={example} scope={{Form: Form, BasicInput: BasicInput, autoBind: autoBind}}>
                         <LiveEditor className="col-md-6"/>
